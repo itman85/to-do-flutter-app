aa1a49918fadde65b09469151f018b34675e5a38
Phan Nguyen <pcnguyen1985@gmail.com>
2018-12-25 17:53:58 +0700

    create to-do app

---
+++ android/app/src/main/java/nguyen/phan/todoapp/MainActivity.java
@@ -0,0 +1,13 @@
+package nguyen.phan.todoapp;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
