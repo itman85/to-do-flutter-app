3a24e4f725e9d830c1cce08e7596d20e452ce936
Phan Nguyen <pcnguyen1985@gmail.com>
2019-01-03 18:17:17 +0700

    update to to screen

--- lib/ui/todo_screen.dart
+++ lib/ui/todo_screen.dart
@@ -17,12 +17,48 @@ class ToDoScreen extends StatefulWidget{
 class ToDoScreenState extends State<ToDoScreen>{
   final TextEditingController _textEditingController = new TextEditingController();
   var db = new DatabaseHelper();
+  final List<ToDoItem> _itemList = <ToDoItem>[];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _readToDoList();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
       backgroundColor: Colors.black87,
-      body: Column(),
+      body: Column(
+        children: <Widget>[
+          new Flexible(
+              child: new ListView.builder(
+                  padding: new EdgeInsets.all(8.0),
+                  reverse: false,
+                  itemCount: _itemList.length,
+                  itemBuilder: (_,int index){
+                    return new Card(
+                      color:Colors.white10,
+                      child: new ListTile(
+                        title: _itemList[index],
+                        onLongPress: ()=> debugPrint(""),
+                        trailing: new Listener(
+                          key: new Key(_itemList[index].itemName),
+                          child: new Icon(Icons.remove_circle,color: Colors.redAccent,),
+                          onPointerDown: (pointerEvent)=> debugPrint(""),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+            new Divider(
+            height: 1.0,
+            )
+          ],
+      ),
       floatingActionButton:  new FloatingActionButton(
         tooltip: "Add Item",
           backgroundColor: Colors.redAccent,
@@ -66,6 +102,7 @@ class ToDoScreenState extends State<ToDoScreen>{
     List items = await db.getItems();
     items.forEach((item){
       ToDoItem toDoItem = ToDoItem.map(item);
+      print("Db items: ${toDoItem.itemName}");
     });
   }
 
@@ -73,7 +110,11 @@ class ToDoScreenState extends State<ToDoScreen>{
     _textEditingController.clear();
     ToDoItem toDoItem = new ToDoItem(text, DateTime.now().toIso8601String());
     int savedItemId = await db.saveItem(toDoItem);
+    ToDoItem addedItem = await db.getItem(savedItemId);
     print("Item saved id $savedItemId");
+    setState(() {
+      _itemList.insert(0,addedItem);
+    });
   }
 
 
